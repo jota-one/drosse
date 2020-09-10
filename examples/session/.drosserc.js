5ea26d3e897de0012c8a4cc875738dd65a5dc69d
Tadai <tadai@jota.one>
2020-09-10 21:13:01 +0200

    Add user session middleware example

---
+++ examples/session/.drosserc.js
@@ -0,0 +1,17 @@
+const session = require('./middlewares/session')
+
+module.exports = {
+  name: 'Example session',
+  middlewares: [session, function(req, res, next) {
+    if (req.url === '/auth' && req.method === 'POST') {
+      return next()
+    }
+
+    if (!req.session.authenticated) {
+      const msg = '<h2>You\'re not authenticated</h2><br>Call <pre style="display:inline">fetch("/auth", { method: "post" })</pre> in the browser\'s console to authenticate and <b>refresh this page</b>'
+      return res.status(401).send(msg)
+    }
+
+    next()
+  }]
+}
---
+++ examples/session/middlewares/session.js
@@ -0,0 +1,8 @@
+const session = require('express-session')
+module.exports = session({
+  secret: 'drosse',
+  cookie: { maxAge: 2 * 60 * 60 * 1000 },
+  resave: false,
+  saveUninitialized: false,
+  httpOnly: false
+})
