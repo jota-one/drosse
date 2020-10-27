5c3b054ae60d29cdbd7979345d167183904dc9e9
Tadai <tadai@jota.one>
2020-10-27 15:50:28 +0100

    Linting

--- examples/session/.drosserc.js
+++ examples/session/.drosserc.js
@@ -3,16 +3,21 @@ const session = require('./middlewares/session')
 module.exports = {
   name: 'Example session',
   port: 8795,
-  middlewares: ['open-cors', session, function(req, res, next) {
-    if (req.url === '/auth' && req.method === 'POST') {
-      return next()
-    }
+  middlewares: [
+    'open-cors',
+    session,
+    function (req, res, next) {
+      if (req.url === '/auth' && req.method === 'POST') {
+        return next()
+      }
 
-    if (!req.session.authenticated) {
-      const msg = '<h2>You\'re not authenticated</h2><br>Call <pre style="display:inline">fetch("/auth", { method: "post" })</pre> in the browser\'s console to authenticate and <b>refresh this page</b>'
-      return res.status(401).send(msg)
-    }
+      if (!req.session.authenticated) {
+        const msg =
+          '<h2>You\'re not authenticated</h2><br>Call <pre style="display:inline">fetch("/auth", { method: "post" })</pre> in the browser\'s console to authenticate and <b>refresh this page</b>'
+        return res.status(401).send(msg)
+      }
 
-    next()
-  }]
+      next()
+    },
+  ],
 }
