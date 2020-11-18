832c635c08b25d5958fc590aa20edd099bab81b8
Tadai <tadai@jota.one>
2020-11-18 20:47:29 +0100

    Enable folders in static files handling

--- app/io.js
+++ app/io.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const path = require('path')
+const rrdir = require('rrdir')
 const { isEmpty } = require('lodash')
 const { v4: uuidv4 } = require('uuid')
-const { replaceExpress } = require('@jota-one/replacer')
+const { replace } = require('@jota-one/replacer')
 const useState = require('./use/state')
 const useMiddleware = require('./use/middlewares')
 const useTemplates = require('./use/templates')
@@ -59,8 +60,17 @@ const loadService = (routePath, verb) => {
   return require(serviceFile)
 }
 
-const loadStatic = (routePath, params = {}, verb = null, skipVerb = false) => {
-  let staticFile = replaceExpress(
+const findStatic = (
+  root,
+  files,
+  routePath,
+  params = {},
+  verb = null,
+  skipVerb = false
+) => {
+  const comparePath = filePath => filePath.replace(root, '').substr(1)
+
+  let staticFile = replace(
     path.join(
       state.get('root'),
       state.get('staticPath'),
@@ -75,24 +85,50 @@ const loadStatic = (routePath, params = {}, verb = null, skipVerb = false) => {
     staticFile.slice(-5) === '.json' ? '' : '.json'
   )
 
-  if (!fs.existsSync(staticFile)) {
-    logger.error(`loadStatic: tried with [${staticFile}]. File not found.`)
+  const foundFiles = files.filter(
+    file =>
+      comparePath(file.path).replace(/\//gim, '.') === comparePath(staticFile)
+  )
+
+  if (foundFiles.length > 1) {
+    const error = `findStatic: more than 1 file found for:\n[${staticFile}]:\n${foundFiles
+      .map(f => f.path)
+      .join('\n')}`
+
+    logger.error(error)
+    return { drosse: error }
+  }
+
+  if (foundFiles.length === 1) {
+    staticFile = foundFiles[0].path
+    logger.info(`findStatic: file used: ${staticFile}`)
+  }
+
+  if (foundFiles.length === 0) {
+    logger.error(`findStatic: tried with [${staticFile}]. File not found.`)
 
     if (verb && !skipVerb) {
-      return loadStatic(routePath, params, verb, true)
+      return findStatic(root, files, routePath, params, verb, true)
     }
 
     // retry by removing the first param of the list
     if (!isEmpty(params)) {
-      logger.error(`loadStatic: tried with [${staticFile}]. File not found.`)
+      logger.error(`findStatic: tried with [${staticFile}]. File not found.`)
       const newParams = Object.keys(params)
         .slice(1)
         .reduce((acc, name) => ({ ...acc, [name]: params[name] }), {})
-      return loadStatic(routePath, newParams, verb)
+      return findStatic(root, files, routePath, newParams, verb)
     }
     return { drosse: `file [${staticFile}] not found.` }
   }
+
   return require(staticFile)
+}
+
+const loadStatic = (routePath, params = {}, verb = null, skipVerb = false) => {
+  const root = path.join(state.get('root'), state.get('staticPath'))
+  const files = rrdir.sync(root)
+  return findStatic(root, files, routePath, params, verb, skipVerb)
 }
 
 const loadUuid = () => {
