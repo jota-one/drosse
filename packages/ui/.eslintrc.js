703b645c9db0f1b15097c71b50422ae0ff5021bd
Jorinho <jorinho@jota.one>
2021-01-12 14:41:04 +0100

    moved drosse into core package + configured lerna

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/recommended',
     'eslint:recommended',
     '@vue/standard',
-    '@vue/prettier'
+    '@vue/prettier',
   ],
   parserOptions: {
     // Needed for dynamic imports
     // => https://stackoverflow.com/questions/47815775/dynamic-imports-for-code-splitting-cause-eslint-parsing-error-import
     parser: 'babel-eslint',
     sourceType: 'module',
-    ecmaVersion: 8
+    ecmaVersion: 8,
   },
   rules: {
     'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-v-html': 0,
-    'vue/no-v-html': 0
+    'vue/no-v-html': 0,
   },
-  plugins: ['prettier']
+  plugins: ['prettier'],
 }
