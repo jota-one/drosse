e338294e52244cdc90212407d5a79fb84629aee4
Tadai <tadai@jota.one>
2021-05-21 16:46:35 +0200

    feat(www with zola+svelte): terminal cmpt done

--- packages/www/frontend/src/components/NpmButton.svelte
+++ packages/www/frontend/src/components/NpmButton.svelte
@@ -1,48 +1,54 @@
 <script>
-    import Icon from './Icon.svelte'
+  import Icon from './Icon.svelte'
 
-    export let command = ''
+  export let command = ''
+
 </script>
 
-<button on:click={() => { navigator.clipboard.writeText(command) }}>
-    <code>{ command }</code>
-    <Icon name="copy" size="20"/>
+<button
+  on:click={() => {
+    navigator.clipboard.writeText(command)
+  }}
+>
+  <code>{command}</code>
+  <Icon name="copy" size="20" />
 </button>
 
 <style lang="postcss">
-    button {
-        display: flex;
-        align-items: center;
-        padding: 0.75rem;
-        white-space: nowrap;
-        font-size: 0.8rem;
-        color: rgb(80,80,80);
-        border: 2px solid rgba(128,128,128, 0.25);
-        background: rgba(128,128,128, 0.1);
-        border-radius: 0.3rem;
-        cursor: pointer;
+  button {
+    display: flex;
+    align-items: center;
+    padding: 0.75rem;
+    white-space: nowrap;
+    font-size: 0.8rem;
+    color: rgb(80, 80, 80);
+    border: 2px solid rgba(128, 128, 128, 0.25);
+    background: rgba(128, 128, 128, 0.1);
+    border-radius: 0.3rem;
+    cursor: pointer;
 
-        :global(.dark) & {
-            color: rgb(160,160,160);
-            border-color: rgba(128,128,128, 0.35);
-            background: rgb(25,25,25);
+    :global(.dark) & {
+      color: rgb(160, 160, 160);
+      border-color: rgba(128, 128, 128, 0.35);
+      background: rgb(25, 25, 25);
 
-            &:hover code {
-                color: rgb(160,160,160);
-            }
-        }
-
-        &:hover {
-            color: var(--c-green);
-            border-color: var(--c-green);
-
-            code {
-                color: rgb(80,80,80);
-            }
-        }
+      &:hover code {
+        color: rgb(160, 160, 160);
+      }
     }
 
-    code {
-        margin: 0 0.75rem -0.125rem 0;
+    &:hover {
+      color: var(--c-green);
+      border-color: var(--c-green);
+
+      code {
+        color: rgb(80, 80, 80);
+      }
     }
+  }
+
+  code {
+    margin: 0 0.75rem -0.1rem 0;
+  }
+
 </style>
--- packages/www/frontend/src/components/Terminal.svelte
+++ packages/www/frontend/src/components/Terminal.svelte
@@ -1,155 +1,207 @@
 <script>
-    import { onMount } from 'svelte';
-    import Typed from 'typed.js'
+  import { onMount } from 'svelte'
+  import Typed from 'typed.js'
 
-    export let steps = ''
-    let shell
-    const commands = []
+  export let steps = ''
+  let currentCommand = 0
+  let shell
 
-    const typedOptions = {
-        typeSpeed: 30,
-        loop: false,
-        startDelay: 1000,
-        onComplete: function() {
-            console.log('typed complete')
-        }
+  $: _steps = JSON.parse(steps)
+
+  const typedOptions = {
+    typeSpeed: 30,
+    loop: false,
+    startDelay: 1000,
+  }
+
+  onMount(async () => {
+    for (const step of _steps) {
+      await execute(step)
+      currentCommand++
     }
+  })
 
-    onMount(() => {
-        console.log('mounted', steps)
-    })
+  function execute(step) {
+    return new Promise(resolve => {
+      const cmdEl = shell.querySelector(
+        `.command:nth-child(${currentCommand + 1})`
+      )
 
-    function pad(input) {
-          var str = input.toString()
-          try {
-            return str.padStart(2, '0')
-          } catch (e) {
-            return str
+      const el = cmdEl.querySelector('.typed')
+
+      const options = {
+        ...typedOptions,
+        ...step.options,
+        strings: [step.command],
+        onComplete: onTypedCommplete,
+      }
+
+      async function onTypedCommplete(self) {
+        if (!options.loop) {
+          self.el.parentElement.querySelector('.typed-cursor').style.display =
+            'none'
+        }
+
+        let i = 0
+
+        const interval = window.setInterval(() => {
+          if (i === step.output.length - 1) {
+            window.clearInterval(interval)
+            resolve(true)
           }
-        }
 
-    function now() {
-        const d = new Date()
-        return `${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`
+          const lineEl = cmdEl.querySelector(`.line-${i}`)
+          lineEl.classList.add('show')
+          lineEl.innerHTML = lineEl.innerHTML.replace('js:now', now())
+          i++
+        }, 50)
+      }
+
+      cmdEl.classList.add('show')
+      new Typed(el, options)
+    })
+  }
+
+  function pad(input) {
+    var str = input.toString()
+    try {
+      return str.padStart(2, '0')
+    } catch (e) {
+      return str
     }
+  }
+
+  function now() {
+    const d = new Date()
+    return `${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`
+  }
+
 </script>
 
 <div class="terminal">
-    <div class="window">
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-    <div class="shell" bind:this={shell}>
-    {#each JSON.parse(steps) as _, i}
-        <code class="command">
-            <span class="green">$</span> <span class="command-{i}"></span>
-        </code>
-        <!--{#each step.output as line}
-            <code>
-                {@html line.replace('js:now', now())}
-            </code>
-        {/each}-->
+  <div class="window">
+    <div />
+    <div />
+    <div />
+  </div>
+  <div class="shell" bind:this={shell}>
+    {#each JSON.parse(steps) as step}
+      <code class="command">
+        <span class="green">$</span> <span class="typed" />
+        {#each step.output as line, i}
+          <span class="line line-{i}">{@html line}</span>
+        {/each}
+      </code>
     {/each}
-    </div>
+  </div>
 </div>
 
 <style lang="postcss">
-    @import "../styles/_media.pcss";
-    
-    .terminal {
-        position: relative;
-        width: 100%;
-        background-color: rgba(245,245,245, 0.9);
-        border: 1px solid rgba(0,0,0, 0.125);
-        border-radius: .35rem;
-        box-shadow: 0 0.25rem 1.5rem rgb(0 0 0 / 20%);
+  @import '../styles/_media.pcss';
 
-        :global(.dark) & {
-            border-color: rgba(255,255,255, 0.125);
-            background-color: rgba(20,20,20, 0.9);
-        }
+  .terminal {
+    position: relative;
+    width: 100%;
+    background-color: rgba(245, 245, 245, 0.9);
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    border-radius: 0.35rem;
+    box-shadow: 0 0.25rem 1.5rem rgb(0 0 0 / 20%);
 
-        &:after {
-            content: '';
-            display: block;
-            height: 0;
-            width: 100%;
-            padding-bottom: 100%;
-
-            @media (--s) {
-                padding-bottom: 75%;
-                min-width: 35rem;
-            }
-
-            @media (--m) {
-                padding-bottom: 62.5%;
-            }
-        }
+    :global(.dark) & {
+      border-color: rgba(255, 255, 255, 0.125);
+      background-color: rgba(10, 10, 10, 0.9);
     }
 
-    .window {
-        position: absolute;
-        top: 0;
-        left: 0;
-        display: flex;
-        align-items: center;
-        padding: .75rem;
+    &:after {
+      content: '';
+      display: block;
+      height: 0;
+      width: 100%;
+      padding-bottom: 100%;
 
-        div {
-            width: 0.65rem;
-            height: 0.65rem;
-            margin-right: 0.375rem;
-            border-radius: 99999px;
-            background: rgb(200,200,200);
+      @media (--s) {
+        padding-bottom: 70%;
+        min-width: 35rem;
+      }
 
-            :global(.dark) & {
-                background: rgb(80,80,80);
-            }
-        }
+      @media (--m) {
+        padding-bottom: 60%;
+      }
     }
+  }
 
-    .shell {
-        position: absolute;
-        top: 1.75rem;
-        bottom: 0;
-        overflow: auto;
-        right: 0;
-        left: 0;
-        padding: 1rem 1.5rem 1.5rem;
-        font-size: 0.8rem;
-        color: rgb(113,113,113);
+  .window {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    padding: 0.75rem;
 
-        :global(.dark) & {
-            color: rgb(160,160,160);
-        }
+    div {
+      width: 0.65rem;
+      height: 0.65rem;
+      margin-right: 0.375rem;
+      border-radius: 50%;
+      background: rgb(200, 200, 200);
+
+      :global(.dark) & {
+        background: rgb(80, 80, 80);
+      }
     }
+  }
 
-    code {
-        display: block;
-    }
+  .shell {
+    position: absolute;
+    top: 1.75rem;
+    bottom: 0;
+    overflow: auto;
+    right: 0;
+    left: 0;
+    padding: 1rem 1.5rem 1.5rem;
+    font-size: 0.825rem;
+    color: rgb(113, 113, 113);
 
-    :global(.blue) {
-        color: rgb(59,130,246);
+    :global(.dark) & {
+      color: rgb(160, 160, 160);
     }
+  }
 
-    :global(.green) {
-        color: rgb(74,222,128);
-    }
+  .command:not(.show),
+  .line:not(.show) {
+    display: none;
+  }
 
-    :global(.pink) {
-        color: rgb(219,39,119);
-    }
+  .line {
+    display: block;
+  }
 
-    :global(.opacity-75) {
-        opacity: 0.75;
-    }
+  code {
+    display: block;
+  }
 
-    :global(.opacity-50) {
-        opacity: 0.5;
-    }
+  :global(.blue) {
+    color: rgb(59, 130, 246);
+  }
 
-    :global(.opacity-25) {
-        opacity: 0.25;
-    }
+  :global(.green) {
+    color: rgb(74, 222, 128);
+  }
+
+  :global(.pink) {
+    color: rgb(219, 39, 119);
+  }
+
+  :global(.opacity-75) {
+    opacity: 0.5;
+  }
+
+  :global(.opacity-50) {
+    opacity: 0.5;
+  }
+
+  :global(.opacity-25) {
+    opacity: 0.25;
+  }
+
 </style>
