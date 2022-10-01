cc5233c25b6c15a3cec99131ab9f16fabb3af8d2
Tadai <tadai@jota.one>
2022-10-01 15:27:29 +0200

    feat(middlewares): adapt examples

--- examples/session/.drosserc.js
+++ examples/session/.drosserc.js
@@ -1,22 +1,8 @@
 const session = require('./middlewares/session')
+const checkSession = require('./middlewares/check-session')
 
 module.exports = {
   name: 'Example session',
   port: 8795,
-  middlewares: [
-    'open-cors',
-    session,
-    function (req, res) {
-      if (req.url === '/auth' && req.method === 'POST') {
-        return
-      }
-
-      if (!req.session.authenticated) {
-        const msg =
-          '<h2>You\'re not authenticated</h2><br>Run <pre style="display:inline">fetch("/auth", { method: "post" })</pre> in the browser\'s console to authenticate and <b>refresh this page</b>'
-        res.statusCode = 401
-        return msg
-      }
-    },
-  ],
+  middlewares: { session, checkSession }
 }
--- examples/sockjs/.drosserc.js
+++ examples/sockjs/.drosserc.js
@@ -3,6 +3,5 @@ const { extendServer } = require('./ws')
 module.exports = {
   name: 'SockJS example',
   port: 8119,
-  middlewares: ['open-cors'],
   extendServer
 }
