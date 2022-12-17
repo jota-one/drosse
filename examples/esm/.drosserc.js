388cee60458bff5fac0037799b176551c9c9e2a8
Tadai <tadai@jota.one>
2022-12-17 11:29:09 +0100

    fix(esm): add drosse-esm bin

--- examples/esm/.drosserc.js
+++ examples/esm/.drosserc.js
@@ -1,5 +1,7 @@
 // This has to be run with the --esm option:
 // `npx @jota-one/drosse serve examples/esm --esm`
+// or with the drosse-esm binary:
+// `npx drosse-esm serve examples/esm`
 
 import { response } from './templates/response'
 
